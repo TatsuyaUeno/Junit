e1e1bc174852fee55e2b526b270d0081bf9fc93c
tatsu <tatsu@ut_soccer222>
2024-10-16 19:47:58 +0900

    first commit

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import java.util.Objects;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String str = new String();
+		
+		System.out.println(Objects.isNull(str));
+	}
+
+}
